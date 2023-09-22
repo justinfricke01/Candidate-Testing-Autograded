de98b546d17cbed3c47dadd0288960ea63ae01ea
unknown <justinfricke01@gmail.com>
2023-09-21 20:12:56 -0500

    anything

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,51 +3,58 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
 let questions;
+ 
 let correctAnswers;
+ 
 let candidateAnswers;
 
 
-function askForName() {
+candidateName = input.question("What is your name?"); 
   // TODO 1.1b: Ask for candidate's name //
 
-}
 
-function askQuestion() {
+
+ candidateAnswer = input.question(question); 
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
 
-}
 
-function gradeQuiz(candidateAnswers) {
+
+let gradeQuiz = ("candidateAnswers" && "correctAnswer"); 
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  if ("gradeQuiz" == true) {
+    console.log("incorrect");
+  } else {
+    console.log('correct');
+  }
 
 
+    //TODO 3.2a use this variable to calculate the candidates score.
+//TODO 3.2b calculate the candidate's percentage
+// let grade = 
+//   console.log(grade);
 
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
-
-
-  return grade;
-}
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
-  askQuestion();
-  gradeQuiz(this.candidateAnswers);
+   console.log("Hello", + "candidateName");
+   askQuestion();
+   gradeQuiz(this.candidateAnswers);
 }
 
-// ----------- Don't write any code or change any code below this line ---------- //
+// Don't write any code below this line //
+// And don't change these or your program will not run as expected //
 module.exports = {
   candidateName: candidateName,
   question: question,
